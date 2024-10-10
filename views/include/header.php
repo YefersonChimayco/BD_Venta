12225f82b3b14e47a97e77a37c8471d675d88ed9
JuanCarlosCatalino <juancarloscatalino534@gmail.com>
2024-10-10 09:47:37 -0500

    CONTINUE

--- views/include/header.php
+++ views/include/header.php
@@ -7,7 +7,7 @@
     , initial-scale=1.0">
   <title>Tazas.com</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href=" <?php echo BASE_URL?> styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 
